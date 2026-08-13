7c3ff3e9e3ecac1279a3d4c3f4bcb8a8af7f00d0
NurAzizah <azizahulnha@gmail.com>
2026-08-14 01:55:40 +0700

    Replace repository URLs with Figma links in socialLinks
    Updated socialLinks.ts to replace repository URLs with Figma design links.

--- src/config/socialLinks.ts
+++ src/config/socialLinks.ts
@@ -5,12 +5,12 @@ export const socialLinks = {
   linkedin: 'https://www.linkedin.com/in/nur-azizah-ulinnuha',
   email: 'azizahulnha@gmail.com',
 
-  // GitHub repository URLs
-  repositories: {
-    projectOne: 'https://github.com/username/project-one',
-    projectTwo: 'https://github.com/username/project-two',
-    projectThree: 'https://github.com/username/project-three',
-    projectFour: 'https://github.com/username/project-four',
+  // repository URLs
+  figma: {
+    shifaaMind: 'https://www.figma.com/design/0MNX4heCL7MypyWjyiipiD/Gemastik-UX-By-Faid--Copy-?m=auto&t=GxNvbpdJa1J9ig6P-6',
+    freshLens: 'https://www.figma.com/design/msdadQv6IAqWevusS5hBg3/FreshLens?m=auto&t=GxNvbpdJa1J9ig6P-6',
+    sepulau: 'https://www.figma.com/design/KAEdd1JdMqBlv7KkkdHKbP/SEPULAU-ID?m=auto&t=GxNvbpdJa1J9ig6P-6',
+    bandingin: 'https://www.figma.com/design/tU6RWSvSFexaKj84hrZOCT/Bandingin.id--Copy-?m=auto&t=GxNvbpdJa1J9ig6P-6',
   },
 
   // Formatted display names
